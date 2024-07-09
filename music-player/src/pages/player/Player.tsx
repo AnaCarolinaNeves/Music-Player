82cbdd0798b4beed47d9c2952b392a0f963b2131
AnaCarolinaNeves <aninha_0512_@hotmail.com>
2024-07-09 20:10:58 -0300

    fix upload routes

--- music-player/src/pages/player/Player.tsx
+++ music-player/src/pages/player/Player.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/header/Header';
 import './player.css';
-import image2 from '../../images/01.png';
-import { Repeat, SkipStart, SkipEnd, Play, MusicNoteList, ThreeDots, ChevronDown, X } from 'react-bootstrap-icons';
+import { Repeat, SkipStart, SkipEnd, Play, MusicNoteList, X } from 'react-bootstrap-icons';
+import { api, URI } from '../../enumerations/uri'; 
+import { Song } from '../../types/song';
 
 function Player() {
+    const [songs, setSongs] = useState<Song[]>([]);
+    const [currentSongIndex, setCurrentSongIndex] = useState<number>(0);
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+    useEffect(() => {
+        const fetchSongs = async () => {
+            try {
+                const response = await api.get<Song[]>(URI.GET_ALL_SONGS);
+                setSongs(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar músicas:', error);
+            }
+        };
+        fetchSongs();
+    }, []);
+
+    useEffect(() => {
+        const audioElement = document.getElementById('main-audio') as HTMLAudioElement;
+        if (audioElement) {
+            audioElement.load(); // Carrega a nova música quando currentSongIndex muda
+        }
+    }, [currentSongIndex]);
+
+    const playPauseHandler = () => {
+        const audioElement = document.getElementById('main-audio') as HTMLAudioElement;
+        if (audioElement) {
+            if (isPlaying) {
+                audioElement.pause();
+            } else {
+                audioElement.play();
+            }
+            setIsPlaying(!isPlaying);
+        }
+    };
+
+    const nextSongHandler = () => {
+        setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songs.length);
+    };
+
+    const prevSongHandler = () => {
+        setCurrentSongIndex((prevIndex) => (prevIndex - 1 + songs.length) % songs.length);
+    };
+
+    const getFullPath = (path: string) => {
+        return `${api.defaults.baseURL}/${path}`;
+    };
+
     return (
         <>
             <div className="player-container">
@@ -12,20 +60,18 @@ function Player() {
                 <div id="body">
                     <div className="wrapper">
                         <div className="top-bar">
-                            {/* <i className="material-icons"><ChevronDown size={32} /></i> */}
                             <span>Now Playing</span>
-                            {/* <i className="material-icons"><ThreeDots size={32} /></i> */}
                         </div>
                         <div className="img-area">
-                            <img src={image2} alt="Song cover" />
+                            <img src={songs[currentSongIndex]?.imgPath ? getFullPath(songs[currentSongIndex].imgPath) : 'default_image_url'} alt="Song cover" />
                         </div>
                         <div className="song-details">
-                            <p className="name">Song Name</p>
-                            <p className="artist">Artist Name</p>
+                            <p className="name">{songs[currentSongIndex]?.title || 'Song Name'}</p>
+                            <p className="artist">{songs[currentSongIndex]?.artista || 'Artist Name'}</p>
                         </div>
                         <div className="progress-area">
                             <div className="progress-bar">
-                                <audio id="main-audio" src=""></audio>
+                                <audio id="main-audio" src={songs[currentSongIndex]?.audioPath ? getFullPath(songs[currentSongIndex].audioPath) : ''}></audio>
                             </div>
                             <div className="song-timer">
                                 <span className="current-time">0:00</span>
@@ -34,11 +80,11 @@ function Player() {
                         </div>
                         <div className="controls">
                             <i id="repeat-plist"><Repeat size={32} title="Playlist looped" /></i>
-                            <i id="prev"><SkipStart size={32} /></i>
-                            <div className="play-pause">
+                            <i id="prev" onClick={prevSongHandler}><SkipStart size={32} /></i>
+                            <div className="play-pause" onClick={playPauseHandler}>
                                 <i className="material-icons play"><Play size={32} /></i>
                             </div>
-                            <i id="next"><SkipEnd size={32} /></i>
+                            <i id="next" onClick={nextSongHandler}><SkipEnd size={32} /></i>
                             <i id="more-music"><MusicNoteList size={32} /></i>
                         </div>
                         <div className="music-list">
@@ -50,7 +96,14 @@ function Player() {
                                 <i id="close"><X size={32} />close</i>
                             </div>
                             <ul>
-                                {/* here li list are coming from js */}
+                                {songs.map((song, index) => (
+                                    <li key={index} onClick={() => setCurrentSongIndex(index)}>
+                                        <div className="row">
+                                            <span>{song.title}</span>
+                                            <p>{song.artista}</p>
+                                        </div>
+                                    </li>
+                                ))}
                             </ul>
                         </div>
                     </div>
